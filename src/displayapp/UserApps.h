5fa673fee112119c592897f6e07a97dd85eeaa81
Michael Adams <me@michdavidadams.com>
2024-05-11 01:37:32 -0400

    apps

--- src/displayapp/UserApps.h
+++ src/displayapp/UserApps.h
@@ -3,9 +3,7 @@
 #include "Controllers.h"
 
 #include "displayapp/screens/Alarm.h"
-#include "displayapp/screens/Dice.h"
 #include "displayapp/screens/Timer.h"
-#include "displayapp/screens/Twos.h"
 #include "displayapp/screens/Tile.h"
 #include "displayapp/screens/ApplicationList.h"
 #include "displayapp/screens/WatchFaceDigital.h"
